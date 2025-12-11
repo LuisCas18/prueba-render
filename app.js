260805599d5acb4563c7ea134e6b8f98dfcb49fb
Luis Eduardo Castro Bautista <luis.cas18@outlook.com>
2025-12-11 04:52:03 -0600

    Enhance scraper with media provider and iframe support
    Added media provider and iframe support to the scraper. Updated User-Agent and response handling to include content type.

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require("express");
-const axios = require("axios");
-
 const metascraper = require("metascraper");
 const description = require("metascraper-description");
 const image = require("metascraper-image");
 const title = require("metascraper-title");
 const urlMetadata = require("metascraper-url");
+const mediaProvider = require("metascraper-media-provider");
+const iframe = require("metascraper-iframe");
+const axios = require("axios");
 
 const scraper = metascraper([
+  iframe(),
+  mediaProvider(),
   description(),
   image(),
   title(),
@@ -28,10 +31,13 @@ app.post("/linkpreview", async (req, res) => {
     const response = await axios.get(url, {
       headers: {
         "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0 Safari/537.36",
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0 Safari/537.36",
+        "Accept-Language": "es-ES,es;q=0.9,en;q=0.8",
+        Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
       },
     });
-
+    
+    const contentType = response.headers["content-type"] || "text/html";
     const metadata = await scraper({
       html: response.data,
       url,
@@ -39,8 +45,16 @@ app.post("/linkpreview", async (req, res) => {
 
     return res.json({
       success: true,
-      result: metadata,
-    });
+       result: {
+          mimetype: contentType,
+          siteData: {
+            url: metadata.url,
+            title: metadata.title,
+            description: metadata.description,
+            image: metadata.image
+          }
+        }
+      });
   } catch (err) {
     return res.json({
       success: false,
